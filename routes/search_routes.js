db54d2c27c72540b6872fdbcf33ecf965f54cfae
Jean-Sébastien Tremblay <jean-seb@total-itech.com>
2013-01-12 10:57:06 -0500

    little more

--- routes/search_routes.js
+++ routes/search_routes.js
@@ -5,7 +5,7 @@ exports.index = function(req, res){
     var q = req.query.q;
     delete req.query.q;
     var facets = req.query;
-    Search.index(q, facets, function(err, results){
+    Search.index(q || '', facets, function(err, results) {
         res.set('Server', 'Nodejs by Winning Team 2012');
         if(err) {
             res.json(500, {error: 'Nooooo :('});
