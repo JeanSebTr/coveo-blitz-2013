e0e2dec82a9620b5725bab38d6b8624b465f442c
jack_dbernier <jdesjardinsbernier@gmail.com>
2013-01-12 17:29:23 +0100

    paging

--- crawler.js
+++ crawler.js
@@ -4,11 +4,24 @@ var async = require('async');
 var nodeio = require('node.io');
 var Indexer = require('./indexer').Indexer;
 
+
+var r = function(self, url, page, callback){
+    self.get(url + '?page=' + page, function(err, data){
+        data = JSON.parse(data);
+        callback(data.content);
+        if(data.lastPage){
+            callback(false);
+        }
+        else{
+            r(self, url, page + 1, callback);
+        }
+    });
+};
+
 var Crawler = function(){
     var _this = this;
     _this.start = function(callback){
         _this.counter = 0;
-        console.log(process.env.START_RUN);
         http.get(process.env.START_RUN, function(res) {
             async.parallel([
                 nodeio.start.bind(nodeio, _this.jobArtists, {}),
@@ -22,23 +35,21 @@ var Crawler = function(){
                 });
             });
         }).on('error', function(e) {
-            console.log("Got error: " + e.message);
+            callback(e);
         });
     };
 
     _this.jobArtists = new nodeio.Job({
         input: function (start, num, callback) {
             var url = process.env.DATA_WEB_SERVICE + '/BlitzDataWebService/artists';
-            this.get(url, function(err, data){
-                data = JSON.parse(data);
-                callback(data.content);
-            });
+            r(this, url, 0, callback);
         },
         run: function (content) {
             _this.counter++;
             var url = process.env.DATA_WEB_SERVICE + '/BlitzDataWebService/artists/' + content.id;
             console.log(url);
             this.get(url, function(err, data){
+                console.log('error', err);
                 try{
                     data = JSON.parse(data);
                     data.type = 'artist';
@@ -46,12 +57,13 @@ var Crawler = function(){
                     this.emit(data);
                 }
                 catch(err){
+                    console.log(content);
+                    console.log(data);
                     return;
                 }
             });
         },
         output: function(artists){
-            console.log(_this.counter);
             this.exit();
         }
     });
@@ -59,17 +71,13 @@ var Crawler = function(){
     _this.jobAlbums = new nodeio.Job({
         input: function (start, num, callback) {
             var url = process.env.DATA_WEB_SERVICE + '/BlitzDataWebService/albums';
-            console.log(url);
-            this.get(url, function(err, data){
-                data = JSON.parse(data);
-                callback(data.content);
-            });
+            r(this, url, 0, callback);
         },
         run: function (content) {
             _this.counter++;
             var url = process.env.DATA_WEB_SERVICE + '/BlitzDataWebService/albums/' + content.id;
-            console.log(url);
             this.get(url, function(err, data){
+                console.log('error', err);
                 try{
                     data = JSON.parse(data);
                     data.type = 'album';
@@ -77,12 +85,13 @@ var Crawler = function(){
                     this.emit(data);
                 }
                 catch(err){
+                    console.log(data);
+                    console.log(content);
                     return;
                 }
             });
         },
         output: function(albums){
-            console.log(_this.counter);
             this.exit();
         }
     });
