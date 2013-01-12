3756cb125d98958c8dec3809d363a4312ac4d790
alaingilbert <alain.gilbert.15@gmail.com>
2013-01-12 10:07:09 -0500

    ...

--- indexer.js
+++ indexer.js
@@ -21,10 +21,4 @@ var tokenizer = function(str) {
 };
 
 
-var test = "This is, if I'm not mistaken, a half-good example of what a less naïve tokenizer (if such a contraption exists) should be able to index! -- For good measure, it should also parse numbers like 1000... We can index python magic methods like __init__, but we will ignore things like -----! Aren't we good?"
-
-
-tokenizer(test);
-
-
 module.exports.Indexer = indexer;
